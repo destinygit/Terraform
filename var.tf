5b1a6fd67b45e9a3c675bc624da1e5f2849c274f
Vhutshilo Destiny <55583484+destinygit@users.noreply.github.com>
2022-02-12 20:01:07 +0200

    Update

--- var.tf
+++ var.tf
@@ -1,10 +1,10 @@
 #Subscription & Tenant Id's
 
 variable "subscription_id" {
-  default = "22c1e88b-0de5-4944-879b-a7f604634492"
+  default = "****-***"
 
 }
 variable "tenant_id" {
-  default = "1e120a60-f9eb-4052-a304-9c9e20d7fcd8"
+  default = "****-*****"
 
 }
